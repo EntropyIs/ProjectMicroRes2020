b07f90c5b6e72a964a4caa527d6775c1361cc03d
James P. Galovic <jpgalovic@gmail.com>
2020-08-04 10:59:55 +1000

    Implement Game Object Collision Detection

--- ProjectMicroRes2020/GameObject.cpp
+++ ProjectMicroRes2020/GameObject.cpp
@@ -2,7 +2,40 @@
 
 #include "ResourceManager.h"
 
+using namespace Entropy;
+
 void GameObject::Draw(SpriteRenderer& renderer)
 {
 	renderer.Draw(position + texture_offset, ResourceManager::getTexture(texture_name), sprite_index, ResourceManager::getSpriteSizeData(texture_name));
+}
+
+void GameObject::Update()
+{
+	position += velocity * ResourceManager::getTimeElapsed();
+}
+
+void GameObject::setVelocity(Entropy::Math::Vec2 velocity)
+{
+	this->velocity = velocity;
+}
+
+void GameObject::setVelocityX(float velocity)
+{
+	this->velocity.X = velocity;
+}
+
+void GameObject::setVelocityY(float velocity)
+{
+	this->velocity.X = velocity;
+}
+
+bool GameObject::detectCollions(GameObject& other)
+{
+	Math::Vec2 otherlower = other.position - other.box_offset;
+	Math::Vec2 otherUpper = otherlower + other.box_size;
+	Math::Vec2 lower = position - box_offset;
+	Math::Vec2 upper = lower + box_size;
+
+	return (lower.X < otherUpper.Y&& upper.X > otherlower.X &&
+		lower.Y < otherUpper.Y&& upper.Y > otherlower.Y);
 }
--- ProjectMicroRes2020/GameObject.h
+++ ProjectMicroRes2020/GameObject.h
@@ -10,7 +10,7 @@ class GameObject
 {
 public:
 	std::string name;
-	Entropy::Math::Vec2 position, box_size, box_offset, texture_offset, sprite_index;
+	Entropy::Math::Vec2 position, velocity, box_size, box_offset, texture_offset, sprite_index;
 	std::string texture_name;
 
 	GameObject() {};
@@ -19,8 +19,16 @@ public:
 	GameObject(const char* name, const char* spriteName, Entropy::Math::Vec2 spriteIndex = (0.0f, 0.0f),
 		Entropy::Math::Vec2 position = Entropy::Math::Vec2(0.0f, 0.0f), Entropy::Math::Vec2 boxSize = Entropy::Math::Vec2(8.0f, 8.0f),
 		Entropy::Math::Vec2 boxOffset = Entropy::Math::Vec2(4.0f, 4.0f), Entropy::Math::Vec2 textureOffset = Entropy::Math::Vec2(0.0f, 0.0f)) :
-		name(name), texture_name(spriteName), sprite_index(spriteIndex), position(position),
+		name(name), texture_name(spriteName), sprite_index(spriteIndex), position(position), velocity(0.0f, 0.0f),
 		box_size(boxSize), box_offset(boxOffset), texture_offset(textureOffset) {};
 
-	void Draw(SpriteRenderer& renderer);
+	virtual void Draw(SpriteRenderer& renderer);
+
+	virtual void Update();
+
+	void setVelocity(Entropy::Math::Vec2 velocity);
+	void setVelocityX(float velocity);
+	void setVelocityY(float velocity);
+
+	bool detectCollions(GameObject& other);
 };
